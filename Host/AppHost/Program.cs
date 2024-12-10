08970dae4177aa3869cb6501c8998a1754092230
Гуськов Захар <z.guskov@skbkontur.ru>
2024-12-10 18:35:25 +0500

    add: register default resources in aspire

--- Host/AppHost/Program.cs
+++ Host/AppHost/Program.cs
@@ -1,3 +1,18 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var userNameDefault = builder.AddParameter("username", secret: true);
+var passwordDefault = builder.AddParameter("password", secret: true);
+
+var rabbitMq = builder.AddRabbitMQ("rabbitMq", userNameDefault, passwordDefault)
+    .WithManagementPlugin()
+    .WithDataVolume();
+
+var keycloak = builder.AddKeycloak("keycloak", port: 10001, userNameDefault, passwordDefault)
+    .WithDataVolume();
+
+var postgres = builder.AddPostgres("postgres", userNameDefault, passwordDefault)
+    .WithDataVolume();
+
+var postgresTasks = postgres.AddDatabase("postgresTasks", "tasks");
+
 builder.Build().Run();
