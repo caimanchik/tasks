2e26f80d8eea2b4b37450086db81038d4262370a
Гуськов Захар <z.guskov@skbkontur.ru>
2024-12-10 18:33:11 +0500

    update: customSerilog registration

--- BuildingBlocks/Logging/Extension.cs
+++ BuildingBlocks/Logging/Extension.cs
@@ -2,6 +2,7 @@ using System.Text;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using Serilog.Events;
 using Serilog.Exceptions;
@@ -11,42 +12,62 @@ namespace BuildingBlocks.Logging;
 
 public static class Extension
 {
-    public static WebApplicationBuilder AddCustomSerilog(this WebApplicationBuilder builder, IWebHostEnvironment env)
+    public static WebApplicationBuilder UseCustomSerilog(this WebApplicationBuilder builder, IWebHostEnvironment env)
     {
-        builder.Host.UseSerilog((context, services, loggerConfiguration) =>
+        builder.Host.UseSerilog((context, _, loggerConfiguration) =>
         {
-            var logOptions = context.Configuration.GetSection(nameof(LogOptions)).Get<LogOptions>();
-
-            var logLevel = Enum.TryParse<LogEventLevel>(logOptions!.Level, true, out var level)
-                ? level
-                : LogEventLevel.Information;
-
-            loggerConfiguration
-                .MinimumLevel.Is(logLevel)
-                .WriteTo.SpectreConsole(logOptions.LogTemplate, logLevel)
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-                .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Error)
-                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
-                .MinimumLevel.Override("Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware", LogEventLevel.Fatal)
-                .Enrich.WithExceptionDetails()
-                .Enrich.FromLogContext()
-                .ReadFrom.Configuration(context.Configuration);
-
-            if (logOptions.File.Enabled)
-            {
-                var root = env.ContentRootPath;
-                Directory.CreateDirectory(Path.Combine(root, "logs"));
-
-                var path = string.IsNullOrWhiteSpace(logOptions.File.Path) ? "logs/.txt" : logOptions.File.Path;
-                if (!Enum.TryParse<RollingInterval>(logOptions.File.Interval, true, out var interval))
-                {
-                    interval = RollingInterval.Day;
-                }
-
-                loggerConfiguration.WriteTo.File(path, rollingInterval: interval, encoding: Encoding.UTF8, outputTemplate: logOptions.LogTemplate);
-            }
+            ApplySerilogDefaultConfigureLogger(context.Configuration, loggerConfiguration, env.WebRootPath);
         });
 
         return builder;
+    }
+
+    public static IServiceCollection AddCustomSerilog(this IServiceCollection services, IWebHostEnvironment env)
+    {
+        services.AddSerilog((serviceProvider, loggerConfiguration) =>
+        {
+            var configuration = serviceProvider.GetService<IConfiguration>();
+            ArgumentNullException.ThrowIfNull(configuration);
+            ApplySerilogDefaultConfigureLogger(configuration, loggerConfiguration, env.WebRootPath);
+        });
+
+        return services;
+    }
+
+    private static void ApplySerilogDefaultConfigureLogger(
+        IConfiguration configuration,
+        LoggerConfiguration loggerConfiguration,
+        string rootPath)
+    {
+        var logOptions = configuration.GetSection(nameof(LogOptions)).Get<LogOptions>();
+
+        var logLevel = Enum.TryParse<LogEventLevel>(logOptions!.Level, true, out var level)
+            ? level
+            : LogEventLevel.Information;
+
+        loggerConfiguration
+            .MinimumLevel.Is(logLevel)
+            .WriteTo.SpectreConsole(logOptions.LogTemplate, logLevel)
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Error)
+            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware", LogEventLevel.Fatal)
+            .Enrich.WithExceptionDetails()
+            .Enrich.FromLogContext()
+            .ReadFrom.Configuration(configuration);
+
+        if (logOptions.File.Enabled)
+        {
+            Directory.CreateDirectory(Path.Combine(rootPath, "logs"));
+
+            var path = string.IsNullOrWhiteSpace(logOptions.File.Path) ? "logs/.txt" : logOptions.File.Path;
+            if (!Enum.TryParse<RollingInterval>(logOptions.File.Interval, true, out var interval))
+            {
+                interval = RollingInterval.Day;
+            }
+
+            loggerConfiguration.WriteTo.File(path, rollingInterval: interval, encoding: Encoding.UTF8,
+                outputTemplate: logOptions.LogTemplate);
+        }
     }
 }
