68c3a184fc87c57bb87536bc3cb5128dbbbb2927
Nikita_Trofimov <nikita174trofimov@gmail.com>
2025-01-03 20:59:34 +0500

    fix

--- Services/Tasks/src/Api.Tasks/ApiModels/TaskEntities/Create/SumOfDigits/SumOfDigitsTaskCreateDto.cs
+++ Services/Tasks/src/Api.Tasks/ApiModels/TaskEntities/Create/SumOfDigits/SumOfDigitsTaskCreateDto.cs
@@ -4,5 +4,5 @@ namespace Api.Tasks.ApiModels.TaskEntities.Create.SumOfDigits;
 
 public class SumOfDigitsTaskCreateDto : TaskCreateDtoBase<SumOfDigitsArtefactsCreateDto>
 {
-public override required SumOfDigitsArtefactsCreateDto Artefacts { get; set; }
+    public override required SumOfDigitsArtefactsCreateDto Artefacts { get; set; }
 }
